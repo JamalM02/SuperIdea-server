bd14e132a8e1d6b8df8edddc051621d2b95c5a3b
Jamal Majadle <Jamal.majadle02@students.kinneret.ac.il>
2024-08-15 11:42:59 +0300

    correcting mongo env

--- services/initAdmin.js
+++ services/initAdmin.js
@@ -8,8 +8,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
+            useUnifiedTopology: true
         });
         console.log('MongoDB connected');
     } catch (error) {
