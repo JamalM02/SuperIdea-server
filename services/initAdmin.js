b7f28648aed6fea97848b4c06290fbb3e8657ad6
Jamal Majadle <Jamal.majadle02@students.kinneret.ac.il>
2024-08-15 11:40:14 +0300

    correcting mongo env

--- services/initAdmin.js
+++ services/initAdmin.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
